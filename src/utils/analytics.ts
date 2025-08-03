24f25e3a737ffcab014dad2b31988a0639317986
Kunal Dhaigude <dhaigudekunal2@gmail.com>
2025-08-03 13:52:52 +0530

    Update Google Analytics Measurement ID placeholder

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -9,7 +9,7 @@ declare global {
 // Track page views
 export const trackPageView = (page: string) => {
   if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('config', 'YOUR_MEASUREMENT_ID_HERE', {
+    window.gtag('config', 'G-8FM80H0X37', {
       page_path: page,
     });
   }
